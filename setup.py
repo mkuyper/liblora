77718415ae569d2e182146146cf1140f74959d98
Michael Kuyper <mkuyper@mkdata.ch>
2020-09-16 22:19:07 -0400

    add pycryptodome as dependency, bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import setuptools
 
 setuptools.setup(
         name="liblora",
-        version="0.0.2",
+        version="0.0.3",
         author="Michael Kuyper",
         author_email="mkuyper@mkdata.ch",
         description="A library with functions related to LoRaWAN, derived from Basic MAC",
         url="https://github.com/mkuyper/liblora",
         packages=setuptools.find_packages(),
+        install_requires=[ "pycryptodome" ],
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: BSD License",
